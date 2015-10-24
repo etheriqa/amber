f95992430f5b99c26f60541108af980a90a20725
Kaede Takamori <etheriqa@gmail.com>
2015-10-25 00:10:39 +0900

    Add copyright header

--- main.cc
+++ main.cc
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2015 Kaede Takamori <etheriqa@gmail.com>
+ *
+ * This file is part of the amber (https://github.com/etheriqa/amber).
+ *
+ * This software is released under the MIT License (the "LICENSE").
+ */
+
 #include "cli/application.h"
 
 int main(int argc, char **argv) {
--- scene/cornel_box.h
+++ scene/cornel_box.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2015 Kaede Takamori <etheriqa@gmail.com>
+ *
+ * This file is part of the amber (https://github.com/etheriqa/amber).
+ *
+ * This software is released under the MIT License (the "LICENSE").
+ */
+
 #pragma once
 
 #include "geometry/primitive/convex_polygon.h"
